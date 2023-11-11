b3c5d771f5a85ae7a838eb7d97ad77b3f2c13423
Aaron Alexander <aaron.3alexander@gmail.com>
2023-11-11 16:40:26 -0600

    pushing for dylan :3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-black'>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
