abfb8036a7cd64a0d1bb0b30358cacb898f86a48
Daniel Ting <danielzting@gmail.com>
2023-11-11 17:17:30 -0600

    Fix index.js

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,25 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 import cors from "cors";
 import session from "express-session";
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-import { MAX_AGE, setTokenCookie, getTokenCookie } from './auth-cookies.js' //for encryption (not used at the moment)
-import { ObjectSchema } from 'yup';
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import "dotenv/config";
 
 const app = express();
 const port = 4000;
 
-// const TOKEN_SECRET = "this-is-a-secret-value-with-at-least-32-characters"; //for encryption (do not uncomment unless working on encryption)
-
 // Connect to MongoDB
-const uri = "mongodb+srv://Roomies:hitbxUCZLGLPXi9E@cluster0.rcurpru.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.MONGO_URI;
 
-mongoose.connect(uri, {
+mongoose
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to your Dylan database');
-})
-.catch((err) => {
-    console.log('Error connecting to database:', err);
-});
-
+  })
+  .then(() => {
+    console.log("Connected to your Dylan database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database:", err);
+  });
