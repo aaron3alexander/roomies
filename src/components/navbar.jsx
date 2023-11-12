db9f8e23e28fecbcc80ae6ad057e8773e315ff7a
Aaron Alexander <aaron.3alexander@gmail.com>
2023-11-12 12:00:02 -0600

    c

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar({ authenticatedUser, setAuthenticatedUser }) {
   return (
     <div className="h-fit w-full flex bg-green-500 justify-between p-4">
       <h1 className="text-white font-display text-2xl overflow-hidden">
-        Roomie
+        Roomies
       </h1>
       <button
         className="w-24 h-8 bg-white rounded-md font-display"
