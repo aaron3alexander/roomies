56733c3a9364a0fc6e62356634dfc2e6a7ef9a39
GivaX <gamma.gamerz.malik@gmail.com>
2023-11-12 04:57:53 -0600

    starting to implement starkent and hardhat for smart cnotracts

---
+++ smart_contracts/starkli-wallets/deployer/hardhat.config.js
@@ -0,0 +1,7 @@
+require("@shardlabs/starknet-hardhat-plugin");
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+};
